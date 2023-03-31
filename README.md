aee8529419996108fbf8c202ad59c0db32dee24d
Bozhidar Slaveykov <bobi@microweber.com>
2023-03-31 18:21:12 +0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # tailwind
-Microweber Tailwind Template
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$config = array();
+$config['name'] = "Tailwind";
+$config['author'] = "Bozhidar Slaveykov (Microweber)";
+$config['version'] = '1.0';
+$config['url'] = "http://microweber.com";
+$config['standalone_module_skins'] = true;
+$config['framework'] = "tailwind";
