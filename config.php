74c2b144f7a01a508feeb45f037d2504351bd991
Bozhidar Slaveykov <bobi@microweber.com>
2023-11-21 17:39:10 +0200

    update

--- config.php
+++ config.php
@@ -6,3 +6,17 @@ $config['version'] = '1.0';
 $config['url'] = "http://microweber.com";
 $config['standalone_module_skins'] = true;
 $config['framework'] = "tailwind";
+
+
+
+$config['settings'] = [];
+$config['settings']['autoload_namespace'] = [
+    [
+        'path' => __DIR__ . '/src/',
+        'namespace' => 'MicroweberPackages\Templates\Tailwind'
+    ],
+];
+
+$config['settings']['service_provider'] = [
+    \MicroweberPackages\Templates\Tailwind\Providers\TailwindServiceProvider::class
+];
---
+++ src/Providers/TailwindServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MicroweberPackages\Templates\Tailwind\Providers;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+class TailwindServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package->name('microweber-templates-tailwind');
+        $package->hasViews('microweber-templates-tailwind');
+    }
+
+    public function register(): void
+    {
+        parent::register();
+    }
+
+}
